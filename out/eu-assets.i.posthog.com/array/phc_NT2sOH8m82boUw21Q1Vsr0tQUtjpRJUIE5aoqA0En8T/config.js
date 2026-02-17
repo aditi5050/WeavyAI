ce52d382907405ad3ed45f2345b5068639b90c04
Aditi5050 <aditigoyalprg21@gmail.com>
2026-02-17 16:12:38 +0530

    removed _archive and out folders as they were not needed

--- out/eu-assets.i.posthog.com/array/phc_NT2sOH8m82boUw21Q1Vsr0tQUtjpRJUIE5aoqA0En8T/config.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
-  window._POSTHOG_REMOTE_CONFIG['phc_NT2sOH8m82boUw21Q1Vsr0tQUtjpRJUIE5aoqA0En8T'] = {
-    config: {"analytics": {"endpoint": "/i/v0/e/"}, "autocaptureExceptions": true, "autocapture_opt_out": false, "captureDeadClicks": false, "capturePerformance": {"network_timing": true, "web_vitals": false, "web_vitals_allowed_metrics": null}, "conversations": false, "defaultIdentifiedOnly": true, "elementsChainAsString": true, "errorTracking": {"autoCaptureControls": {}, "autocaptureExceptions": true, "suppressionRules": []}, "hasFeatureFlags": true, "heatmaps": true, "logs": {"captureConsoleLogs": false}, "productTours": false, "sessionRecording": {"canvasFps": 3, "canvasQuality": "0.4", "consoleLogRecordingEnabled": true, "endpoint": "/s/", "eventTriggers": [], "linkedFlag": "should-record-sessions", "masking": {"maskAllInputs": false}, "minimumDurationMilliseconds": null, "networkPayloadCapture": {"recordBody": true, "recordHeaders": true}, "recordCanvas": true, "recorderVersion": "v2", "sampleRate": null, "scriptConfig": {"script": "posthog-recorder"}, "triggerMatchType": null, "urlBlocklist": [], "urlTriggers": []}, "supportedCompression": ["gzip", "gzip-js"], "surveys": false, "token": "phc_NT2sOH8m82boUw21Q1Vsr0tQUtjpRJUIE5aoqA0En8T"},
-    siteApps: []
-  }
-})();
